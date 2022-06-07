637016e9d22909fd7610ee427104b48ba18456e0
Vlady <contact.johngoldco@gmail.com>
2022-06-07 21:53:28 +0200

    Add more options to playRound function to get all possibilities and Add prompt to playerSelection

--- script.js
+++ script.js
@@ -23,26 +23,55 @@ function computerPlay() {
 // playerSelection should be case-insensitive 
 
 // Run two parameters and compare with each other
-// Player should input value = prompt
 // Compare two results
-// If result is: Player has Rock & Computer has Paper = alert()Computer wins
+// If result is: Player has Rock & Computer has Paper = console.log()Computer wins
 // 
-playerSelection = 'rock';
+playerSelection = prompt('Choose you weapon: ', 'rock');
 computerSelection = computerPlay();
-let winner = ''
+let winner = '';
 
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection == 'Paper') {
+    if (computerSelection == 'Paper' && playerSelection == 'Rock') {
         winner = 'You lose! Paper beats Rock.';
-    } else if (computerSelection == 'Rock') {
+    }
+    else if (computerSelection == 'Paper' && playerSelection == 'Scissors') {
+        winner = 'You won, Congrats!';
+    } else if (computerSelection == 'Paper' && playerSelection == 'Paper') {
         winner = 'It\'s a tie! Try again!';
     }
-    else {
+
+    else if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
+        winner = 'You lose! Rock beats Scissors.';
+    }
+    else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
         winner = 'You won, Congrats!';
+    } else if (computerSelection == 'Rock' && playerSelection == 'Rock') {
+        winner = 'It\'s a tie! Try again!';
     }
 
+
+    else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
+        winner = 'You lose! Scissors beat Paper.';
+    }
+    else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
+        winner = 'You won, Congrats!';
+    } else if (computerSelection == 'Scissors' && playerSelection == 'Scissors') {
+        winner = 'It\'s a tie! Try again!';
+    }
+
+    
     return winner;
 }
 
 console.log(playRound(playerSelection, computerSelection));
+
+
+// Create function game() & put playRound inside & play 5 times
+// Keep score and report a winner at the end
+// Use for loop 
+    //for (let i = 0; i < 5; i++) {
+    // your code here!}
+// Use console.log() to display results and winner at the end
+// Use prompt() to get input from user
+// Feel free to re-work functions and maybe create helper functions
