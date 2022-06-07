056b840a685675f914240a42c091c7fd7dc6d911
Vlady <contact.johngoldco@gmail.com>
2022-06-07 20:19:10 +0200

    Create files & write pseudocode for first instructions

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create function computerPlay which returns 'Rock', 'Paper', or 'Scissors'
+// Use console.log() to test if output is right
+
+// Create function that plays a single round of Rock Paper Scissors.
+// It should take two parameters - playerSelection & computerSelection
+// & return a string declaring the winner e.g.: 'You lose! Paper beats Rock'
+// playerSelection should be case-insensitive 
+
