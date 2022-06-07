0825c0284d0fdb1d6416f99739ba2ce81e834012
Vlady <contact.johngoldco@gmail.com>
2022-06-07 21:22:02 +0200

    Create function for playing one round if player chose rock

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Create function computerPlay which returns 'Rock', 'Paper', or 'Scissors'
 // Use console.log() to test if output is right
-
 function computerPlay() {
     let result = Math.floor(Math.random() * 100) + 1;
     if (result <= 33) {
@@ -15,15 +14,35 @@ function computerPlay() {
     return result;
 }
 
-console.log(computerPlay());
+// console.log(computerPlay());
 
 
-
-
-
-
-// Create function that plays a single round of Rock Paper Scissors.
+// Create function that plays a single round of Rock Paper Scissors
 // It should take two parameters - playerSelection & computerSelection
 // & return a string declaring the winner e.g.: 'You lose! Paper beats Rock'
 // playerSelection should be case-insensitive 
 
+// Run two parameters and compare with each other
+// Player should input value = prompt
+// Compare two results
+// If result is: Player has Rock & Computer has Paper = alert()Computer wins
+// 
+playerSelection = 'rock';
+computerSelection = computerPlay();
+let winner = ''
+
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == 'Paper') {
+        winner = 'You lose! Paper beats Rock.';
+    } else if (computerSelection == 'Rock') {
+        winner = 'It\'s a tie! Try again!';
+    }
+    else {
+        winner = 'You won, Congrats!';
+    }
+
+    return winner;
+}
+
+console.log(playRound(playerSelection, computerSelection));
